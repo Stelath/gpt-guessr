25502d65cb1a630a9bf45cc0e75515db79ab6d3e
Alex Korte <korte@node1399.palmetto.clemson.edu>
2023-02-05 00:30:16 -0500

    Fixed errors

--- GPTGuessr/get_images.py
+++ GPTGuessr/get_images.py
@@ -26,7 +26,6 @@ metadata_url = 'https://maps.googleapis.com/maps/api/streetview/metadata'
 
 def main():
     print('Enter your Google Street View API Key: ')
-    key = input()
     # Open and create all the necessary files & folders
     os.makedirs(args.output, exist_ok=True)
     
@@ -38,15 +37,15 @@ def main():
     coord_output_file = open(os.path.join(args.output, 'picture_coords.csv'), 'w', newline='')
     csv_writer = writer(coord_output_file)
     
-    for i in tqdm(range(args.start_count, args.icount + args.start_count)):
+    for i in tqdm(range(args.start_count//3, (args.icount + args.start_count)//3)):
         # Set the parameters for the API call to Google Street View
         lat, lon = coords[i].y + np.random.randint(10) / 1000, coords[i].x + np.random.randint(10) / 1000
         for j in range(3):
             params = {
-                'key': key,
+                'key': args.key,
                 'size': '640x640',
                 'location': f'{lat:.3f},{lon:.3f}',
-                'heading': str((np.random.randint(0, 3) * 90) + np.random.randint(-15, 15)),
+                'heading': str(j * 120),
                 'pitch': '20',
                 'fov': '90',
                 'radius': 10000000
@@ -58,11 +57,10 @@ def main():
             with open(os.path.join(args.output, filename), "wb") as file:
                 file.write(response.content)
             
-            response = requests.get(url, params)
-            lat, lon = response.json()['location']['lat'], response.json()['location']['lng']
-            
-            # Save the coordinates to the output file
-            csv_writer.writerow([filename, lat, lon])
+        response = requests.get(metadata_url, params)
+        lat, lon = response.json()['location']['lat'], response.json()['location']['lng']
+        # Save the coordinates to the output file
+        csv_writer.writerow([f'street_view_{str(i).zfill(6)}', lat, lon])
 
     coord_output_file.close()
 
