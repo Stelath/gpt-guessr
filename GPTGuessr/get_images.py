2facd810e784eba1ecab929872543d5376c2dbc6
Alex <alex.k.korte@gmail.com>
2023-02-04 23:44:06 -0500

    FIXED

--- GPTGuessr/get_images.py
+++ GPTGuessr/get_images.py
@@ -4,7 +4,10 @@ import os
 import json
 from random import randint
 import argparse
+import numpy as np
 from csv import writer
+import geopandas as gpd
+import pandas as pd
 # Consider using https://osmnx.readthedocs.io/en/stable/osmnx.html#osmnx.utils_geo.sample_points for street gps coords
 # Stack Overflow on how to get the coordinates: https://stackoverflow.com/questions/68367074/how-to-generate-random-lat-long-points-within-geographical-boundaries
 
@@ -12,6 +15,7 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--points", help="The latitude and longitude points to download images from", required=True, type=str)
     parser.add_argument("--output", help="The output folder where the images will be stored, (defaults to: data/)", default='data/', type=str)
+    parser.add_argument("--start-count", help="The starting count for the images (defaults to 0)", default=0, type=int)
     parser.add_argument("--icount", help="The amount of images to pull (defaults to 25,000)", default=25000, type=int)
     parser.add_argument("--key", help="Your Google Street View API Key", type=str, required=True)
     return parser.parse_args()
@@ -20,25 +24,33 @@ args = get_args()
 url = 'https://maps.googleapis.com/maps/api/streetview'
 
 def main():
+    
+    print('Enter your Google Street View API Key: ')
+    key = input()
     # Open and create all the necessary files & folders
     os.makedirs(args.output, exist_ok=True)
     
-    points = np.load(args.points)
+    points = pd.read_pickle(args.points)
+    coords = points.geometry
+    
+    print(f"Downloading off {len(coords)} points")
     
     coord_output_file = open(os.path.join(args.output, 'picture_coords.csv'), 'w', newline='')
     csv_writer = writer(coord_output_file)
     
-    for i in tqdm(range(points.shape[0])):
+    for i in tqdm(range(args.start_count, args.icount + args.start_count)):
         # Set the parameters for the API call to Google Street View
+        lat, lon = coords[i].y + np.random.randint(10) / 1000, coords[i].x + np.random.randint(10) / 1000
         for j in range(3):
             params = {
-                'key': args.key,
+                'key': key,
                 'size': '640x640',
-                'location': str(points[i][0]) + ',' + str(points[i][0]),
-                'heading': str(j * 90),
+                'location': f'{lat:.3f},{lon:.3f}',
+                'heading': str((np.random.randint(0, 3) * 90) + np.random.randint(-15, 15)),
                 'pitch': '20',
-                'fov': '90'
-                }
+                'fov': '90',
+                'radius': 10000000
+            }
             
             response = requests.get(url, params)
             filename = f'street_view_{str(i).zfill(6)}_{j}.jpg'
@@ -47,14 +59,9 @@ def main():
                 file.write(response.content)
             
             # Save the coordinates to the output file
-            csv_writer.writerow([filename, addressLoc[1], addressLoc[0]])
+            csv_writer.writerow([filename, lat, lon])
 
     coord_output_file.close()
-    
-    for i in range(len(cities_count)):
-        city_count = cities_count[i]
-        city_name = os.listdir(args.cities)[i]
-        print(f'{city_count} images pulled from {city_name}')
 
 if __name__ == '__main__':
     main()
