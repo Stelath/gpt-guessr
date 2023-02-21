76d4d79e82dac036f5e958b0dd8ff4ee3ffd5716
Alex <alex.k.korte@gmail.com>
2023-02-21 16:07:21 -0500

    Added Dataset Repair

--- GPTGuessr/gen_dataframe.py
+++ GPTGuessr/gen_dataframe.py
@@ -2,6 +2,7 @@ import os
 import argparse
 import pandas as pd
 from geopy.geocoders import Nominatim
+from tqdm.auto import tqdm
 
 def get_args():
     parser = argparse.ArgumentParser()
@@ -9,9 +10,7 @@ def get_args():
     parser.add_argument("--output", help="The output folder where the dataframe will be stored, (defaults to: data/)", default='data/', type=str)
     return parser.parse_args()
 
-geolocator = Nominatim(user_agent="geoapiExercises")
-
-def city_state_country(row):
+def city_state_country(row, geolocator, pbar):
     coord = f"{row['lat']}, {row['lon']}"
     location = geolocator.reverse(coord, exactly_one=True)
     address = location.raw['address']
@@ -21,12 +20,18 @@ def city_state_country(row):
     row['city'] = city
     row['state'] = state
     row['country'] = country
+    pbar.update(1)
     return row
 
 def main():
     args = get_args()
     df = pd.read_csv(args.dataset, header=None, names=['lat', 'lon'])
-    df = df.apply(city_state_country, axis=1)
+    
+    geolocator = Nominatim(user_agent="geoapiExercises")
+    pbar = tqdm(total=len(df))
+    
+    df = df.apply(city_state_country, args=(geolocator, pbar), axis=1)
+    
     df.to_pickle(os.path.join(args.out, 'dataset.df'), index=False)
 
 if __name__ == '__main__':
--- GPTGuessr/gen_dataset.py
+++ GPTGuessr/gen_dataset.py
@@ -4,24 +4,53 @@ import argparse
 import requests
 import numpy as np
 from tqdm import tqdm
-# Consider using https://osmnx.readthedocs.io/en/stable/osmnx.html#osmnx.utils_geo.sample_points for street gps coords
-# Stack Overflow on how to get the coordinates: https://stackoverflow.com/questions/68367074/how-to-generate-random-lat-long-points-within-geographical-boundaries
+from PIL import Image
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--points", help="The latitude and longitude points csv file to download images from", required=True, type=str)
+    parser.add_argument("--points", help="The latitude and longitude points csv file to download images from", type=str)
     parser.add_argument("--output", help="The output folder where the images will be stored, (defaults to: data/)", default='data/', type=str)
     parser.add_argument("--start-count", help="The starting count for the images (defaults to 0)", default=0, type=int)
     parser.add_argument("--icount", help="The amount of images to pull (defaults to 25,000)", default=25000, type=int)
     parser.add_argument("--key", help="Your Google Street View API Key", type=str, required=True)
+    parser.add_argument("--repair", help="If you want to repair the dataset, set this to true (will only attempt to download grayed out photos)", default=False, type=bool)
     return parser.parse_args()
 
-args = get_args()
 url = 'https://maps.googleapis.com/maps/api/streetview'
 metadata_url = 'https://maps.googleapis.com/maps/api/streetview/metadata'
 
-def main():
-    print('Enter your Google Street View API Key: ')
+def repair_dataset(args):
+    with open(os.path.join(args.output, 'picture_coords.csv'), newline='') as points_file:
+        imgs = list(csv.reader(points_file))
+    
+    for img_set in tqdm(imgs):
+        for i in range(3):
+            filename = os.path.join(args.output, f'{img_set[0]}_{i}.jpg')
+            try:
+                img = Image.open(filename)
+            except:
+                img = None
+            
+            check_sum = np.array(img)[0:10][0:10].sum()
+            if img is None or check_sum == 4339200 or check_sum == 5932800:
+                lat, lon = img_set[1], img_set[2]
+                params = {
+                    'key': args.key,
+                    'size': '640x640',
+                    'location': f'{lat},{lon}',
+                    'heading': str(i * 120),
+                    'pitch': '20',
+                    'fov': '90',
+                    'radius': 50
+                }
+                
+                response = requests.get(url, params)
+                # Save the image to the output folder
+                with open(filename, "wb") as file:
+                    file.write(response.content)
+        
+
+def main(args):
     # Open and create all the necessary files & folders
     os.makedirs(args.output, exist_ok=True)
     
@@ -48,7 +77,7 @@ def main():
             }
             
             response = requests.get(url, params)
-            filename = f'street_view_{str(i).zfill(6)}_{j}.jpg'
+            filename = f'imgs/street_view_{str(i).zfill(6)}_{j}.jpg'
             # Save the image to the output folder
             with open(os.path.join(args.output, filename), "wb") as file:
                 file.write(response.content)
@@ -64,4 +93,8 @@ def main():
     coord_output_file.close()
 
 if __name__ == '__main__':
-    main()
+    args = get_args()
+    if args.repair:
+        repair_dataset(args)
+    else:
+        main(args)
